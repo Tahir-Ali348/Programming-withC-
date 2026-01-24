d6f741c283b2596c05eaad48a7d6d15fb57e2525
Tahir Ali <am6014947@gmail.com>
2026-01-24 20:47:08 +0500

    Variable ,d Constants , Identifires , comments

---
+++ var-cons-com/varcons.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // how to declare variable 
+    int score;
+    score = 110;   //this is one method 
+
+    int balance = 13000;  //    This is the second method to declare in oneline 
+    balance = 20000;
+
+    const int runs = 123;
+    // runs = 90;
+
+    cout << "Welcome to programming with cpp" << endl;
+    cout << "Welcome to programming with cpp 1" << endl;
+    cout << "Welcome to programming with cpp 2" << endl;
+    return 0;
+}
+
+// for single line comment use "//".
+// for multiline comments use /* */.     use "alt + down arrow key" 
