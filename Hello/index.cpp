7c8448b4dbc6951d67b411b7a6e26d2ed3de0b42
Tahir Ali <am6014947@gmail.com>
2026-01-24 20:10:07 +0500

    first program in c++

---
+++ Hello/index.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Hello Tahir Ali" << endl;
+    return 0;
+}
+
+
+
+// Another method to write the code 
+#include <iostream>
+// using namespace std;   --------  instead of this line
+using std::cout; 
+using std::endl; 
+
+int main (){
+    cout << "Hello Tahir Ali" << endl;
+    return 0;
+}
