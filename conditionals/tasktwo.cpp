670ff49106d2be4adc57f2a08fad8941f69cfbdf
Tahir Ali <am6014947@gmail.com>
2026-01-29 19:11:32 +0500

    task two of conditional

---
+++ conditionals/tasktwo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+    int hour;
+    cout << "Enter the current Hour (0 , 23): ";
+    cin >> hour;
+
+    if (hour >= 8 && hour <= 18)
+    {
+        cout << "Shop is OPEN: " << endl;
+    }else{
+    cout << "Shop is CLOSED: " << endl;
+    }
+    
+    return 0;
+}
